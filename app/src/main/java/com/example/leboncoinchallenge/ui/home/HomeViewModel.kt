202a68112c86599278062f3105879f7f786d9c71
Alex <alexandre.louro.de.sousa@gmail.com>
2025-04-26 14:26:32 +0100

    chore(android): Remove unused annotation in test, change the UI, use string resource

--- app/src/main/java/com/example/leboncoinchallenge/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/leboncoinchallenge/ui/home/HomeViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.leboncoinchallenge.ui.home
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.leboncoinchallenge.R
 import com.example.leboncoinchallenge.data.LebonCoinRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
@@ -17,26 +20,30 @@ import javax.inject.Inject
 
 @HiltViewModel
 @OptIn(ExperimentalCoroutinesApi::class)
-class HomeViewModel @Inject constructor(private val repository: LebonCoinRepository) :
-    ViewModel() {
+class HomeViewModel @Inject constructor(
+    @ApplicationContext context: Context, private val repository: LebonCoinRepository
+) : ViewModel() {
 
     val retryTrigger = MutableSharedFlow<Unit>(extraBufferCapacity = 1)
 
-    val uiState: StateFlow<HomeViewState> = retryTrigger
-        .onStart { emit(Unit) } // Emit an initial value to start the flow
-        .flatMapLatest {
-            repository.getAlbums()
-                .map { albums -> HomeViewState.Success(albums) }
-                .onStart<HomeViewState> { emit(HomeViewState.Loading) }
-                .catch { error ->
-                    emit(HomeViewState.Error(errorMessage = "An error occurred ${error.message}"))
-                }
-        }
-        .stateIn(
-            scope = viewModelScope,
-            started = WhileSubscribed(5000),
-            initialValue = HomeViewState.Loading,
-        )
+    val uiState: StateFlow<HomeViewState> =
+        retryTrigger.onStart { emit(Unit) } // Emit an initial value to start the flow
+            .flatMapLatest {
+                repository.getAlbums().map { albums -> HomeViewState.Success(albums) }
+                    .onStart<HomeViewState> { emit(HomeViewState.Loading) }.catch { error ->
+                        emit(
+                            HomeViewState.Error(
+                                errorMessage = context.getString(
+                                    R.string.an_error_occurred, error.message
+                                )
+                            )
+                        )
+                    }
+            }.stateIn(
+                scope = viewModelScope,
+                started = WhileSubscribed(5000),
+                initialValue = HomeViewState.Loading,
+            )
 
     fun retry() {
         retryTrigger.tryEmit(Unit)
